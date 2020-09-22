7e37cbcbd5cc9a97efd3036f0d0e2a1a24269d91
Amanda D <amanda@depaula.xyz>
2020-09-22 17:52:15 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,9 +4,8 @@ count = 0
 while count < src.length do
   element_index = 0
   result = []
-  lowest_value = 50
   while element_index < src[count].length do
-    if src[count][element_index] < lowest_value
+    if src[count][element_index] < src.min
   lowest_value = src[count][element_index]
   end
 element_index += 1

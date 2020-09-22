26e535de66ced84635453c1c81fa2e19ce640654
Amanda D <amanda@depaula.xyz>
2020-09-22 17:55:21 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ while count < src.length do
   element_index = 0
   result = []
   while element_index < src[count].length do
-    if src[count][element_index] < src.min.to_a
+    if src[count][element_index] < src[count][element_index].min
   lowest_value = src[count][element_index]
   end
 element_index += 1
